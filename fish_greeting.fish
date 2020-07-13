0c23c7a904259091d9d665d70457c7f433ab6487
Anselm Schüler <git@anselmschueler.com>
2020-07-14 01:31:32 +0200

    Create fish_greeting.fish

---
+++ fish_greeting.fish
@@ -0,0 +1,13 @@
+function fish_greeting
+    if command -v fortune > /dev/null
+    and command -v cowsay > /dev/null
+    and command -v lolcat > /dev/null
+    and cowsay -f duck "test" | grep "duck" > /dev/null
+        fortune | cowsay -f duck | lolcat
+        echo ""
+        echo "Welcome to fish."
+    else
+        echo ""
+        echo "Hello."
+    end
+end
