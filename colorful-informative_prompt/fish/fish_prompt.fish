42d8fd83c066fe9ea3cad4a22fd8c7f37a52cf94
anselmschueler <git@anselmschueler.com>
2020-09-11 07:47:06 +0200

    Update fish_prompt.fish

--- colorful-informative_prompt/fish/fish_prompt.fish
+++ colorful-informative_prompt/fish/fish_prompt.fish
@@ -1,24 +1,25 @@
 function fish_prompt --description 'Write out the prompt'
     set oldstatus $status
     
-    set red (set_color red)
+    set red (set_color brred)
     set gray (set_color brblack)
     set user_color (set_color $fish_color_cwd)
     set root_color (set_color $fish_color_cwd_root)
+    set path_color (set_color $fish_color_user)
     set normal_color (set_color normal)
 
-    set warning_color $user_color
+    set warning_color $path_color
     if test $oldstatus -gt 0
         set warning_color $red
     end
 
     switch "$USER"
         case root toor
-            echo ┌[(date +%H:%M:%S)] "$root_color""$USER" "$user_color""$PWD" "$warning_color"["$oldstatus"]"$gray" [fish]"$normal_color"
+            echo ┌[(date +%H:%M:%S)] "$root_color""$USER" "$path_color""$PWD" "$warning_color"["$oldstatus"]"$gray" [fish]"$normal_color"
             echo ╘■" "
 
         case '*'
-            echo ┌[(date +%H:%M:%S)] "$user_color""$USER" "$user_color""$PWD" "$warning_color"["$oldstatus"]"$gray" [fish]"$normal_color"
+            echo ┌[(date +%H:%M:%S)] "$user_color""$USER" "$path_color""$PWD" "$warning_color"["$oldstatus"]"$gray" [fish]"$normal_color"
             echo └▶" "
     end
 end
