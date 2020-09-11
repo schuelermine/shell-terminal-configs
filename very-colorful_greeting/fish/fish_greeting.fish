58daa44e00d48124d463f99d895c7065b45b9416
anselmschueler <git@anselmschueler.com>
2020-09-11 07:39:59 +0200

    root/ case in greeting

--- very-colorful_greeting/fish/fish_greeting.fish
+++ very-colorful_greeting/fish/fish_greeting.fish
@@ -1,13 +1,15 @@
 function fish_greeting
-    if command -v fortune > /dev/null
-    and command -v cowsay > /dev/null
-    and command -v lolcat > /dev/null
-    and cowsay -l | grep "duck" > /dev/null
-        fortune | cowsay -f duck | lolcat
-        echo ""
-        echo "Welcome to fish."
-    else
-        echo ""
-        echo "Hello."
+    switch "$USER"
+        case root toor
+            echo "Hello."
+        case '*'
+            if command -v fortune > /dev/null
+            and command -v cowsay > /dev/null
+            and command -v lolcat > /dev/null
+            and cowsay -l | grep "duck" > /dev/null
+                fortune | cowsay -f duck | lolcat
+            end
+            echo ""
+            echo "Welcome to fish."
     end
 end
