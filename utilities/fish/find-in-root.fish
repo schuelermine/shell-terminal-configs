48fc58db8ce38d4f37428de15c6de56a6655c4a3
Anselm Schüler <git@anselmschueler.com>
2020-12-25 21:41:12 +0100

    Update find-in-root.fish

--- utilities/fish/find-in-root.fish
+++ utilities/fish/find-in-root.fish
@@ -1,5 +1,5 @@
 function find-in-root --description "Finds files in the root, excluding mounted filesystems and home directories"
-    for dir in (ls / -1)
+    for dir in (command ls / -1)
         if test $dir != "mnt"; and test $dir != "media"; and test $dir != "home"
             find "/$dir" 2>/dev/null | grep $argv
         end
