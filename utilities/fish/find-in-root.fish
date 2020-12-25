9370be275f100ee179a814ef5564afb8c4ceb764
Anselm Schüler <git@anselmschueler.com>
2020-12-25 21:39:52 +0100

    change formatting because I think fish_indent is wrong

--- utilities/fish/find-in-root.fish
+++ utilities/fish/find-in-root.fish
@@ -1,8 +1,6 @@
 function find-in-root --description "Finds files in the root, excluding mounted filesystems and home directories"
     for dir in (ls / -1)
-        if test $dir != "mnt"
-            and test $dir != "media"
-            and test $dir != "home"
+        if test $dir != "mnt"; and test $dir != "media"; and test $dir != "home"
             find "/$dir" 2>/dev/null | grep $argv
         end
     end
