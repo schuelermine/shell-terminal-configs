a69da4c8cc5042c2889621ccf1992449c22046b2
Anselm Schüler <git@anselmschueler.com>
2020-12-25 21:38:14 +0100

    fish_indent find-in-root.fish

--- utilities/fish/find-in-root.fish
+++ utilities/fish/find-in-root.fish
@@ -1,1 +1,9 @@
-function find-in-root --description "Finds files in the root, excluding mounted filesystems and home directories"; for dir in (ls / -1); if test $dir != "mnt"; and test $dir != "media"; and test $dir != "home"; find "/$dir" 2>/dev/null | grep $argv; end; end; end
+function find-in-root --description "Finds files in the root, excluding mounted filesystems and home directories"
+    for dir in (ls / -1)
+        if test $dir != "mnt"
+            and test $dir != "media"
+            and test $dir != "home"
+            find "/$dir" 2>/dev/null | grep $argv
+        end
+    end
+end
